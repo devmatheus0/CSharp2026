f4dce29eda7f36cd046e56da4b1c79c598c2fdb4
devmatheus0 <devmatheus0@gmail.com>
2026-02-23 19:18:56 -0300

    SEM TRY E CATCH

---
+++ Logica de Programacao e Algoritmos/Aula 06/medidas.cs
@@ -0,0 +1,18 @@
+using static System.Console;
+//Declaração de variáveis
+double a, b, c, areaQuadrado, areaTriangulo, areaTrapezio;
+//Entrada de dados
+Write("Digite a medida A:");
+a = double.Parse(ReadLine());
+Write("Digite a medida B:");
+b = double.Parse(ReadLine());
+Write("Digite a medida C:");
+c = double.Parse(ReadLine());
+//Processamento de dados
+areaQuadrado = a*a;
+areaTriangulo = (a * b)/2;
+areaTrapezio = ((a+b) * c)/2;
+//Saida de dados
+WriteLine($"Area do quadrado é igual a: {areaQuadrado:F4}");
+WriteLine($"Area do triangulo é igual a: {areaTriangulo:F4}");
+WriteLine($"Area do trapezio é igual a: {areaTrapezio:F4}");
