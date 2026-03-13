a92436b7ab56b61978f4d20e683b7aaa122cfcdf
devmatheus0 <devmatheus0@gmail.com>
2026-03-13 20:42:05 -0300

    Fim logica

---
+++ Logica de Programacao e Algoritmos/Aula 13/diagonal.cs
@@ -0,0 +1,30 @@
+/*using static System.Console;
+//Entrada de dados
+Write("Digite o tamanho da matriz: ");
+int n = int.Parse(ReadLine());
+if (n >= 10)
+{
+    Write ("O valor digitado não se aplica para a matriz!");
+}
+
+//Declaração da Matriz
+int [,] matriz = new int [n,n];
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        Write($"Digite o valor p/ a posição [{i},{j}]: ");
+        matriz[i, j] = int.Parse(ReadLine());
+    }
+}
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        Write(matriz[i,j] + "\t");
+    }
+    WriteLine();
+}
+
+Write ($"Os valores negativos são: {matriz [0,0]}");  */
+
