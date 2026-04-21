c71a8569f0297f3c3266411cc017214ec1a168f6
Matheus Rodrigues <devmatheus0@gmail.com>
2026-04-21 16:54:06 -0300

    Add files via upload

---
+++ POO/Aula 11/Funcionarios/Dados.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+namespace funcionario
+{
+    internal class Dados
+    {
+        public string nome;
+        public double salario;
+        public double imposto;
+
+
+        //Construtor
+        public Dados(string nome, double salario, double imposto, double porcentagem)
+        {
+            this.nome = nome;
+            this.salario = salario;
+            this.imposto = imposto;
+
+        }
+
+        public Dados(string nome, double salario, double imposto)
+        {
+            this.nome = nome;
+            this.salario = salario;
+            this.imposto = imposto;
+        }
+
+        //Métodos
+        public string nome_funcionario(string nome)
+        {
+            return nome;
+        }
+        public double salario_funcionario(double salario)
+        {
+            return salario;
+        }
+        public double imposto_salario(double imposto)
+        {
+            return imposto;
+        }
+
+        public double aumento_porcentagem(double porcentagem)
+        {
+            return salario * porcentagem / 100;
+        }
+
+        public double salario_liquido(double salario)
+        {
+            return salario - imposto;
+        }
+
+
+
+        public string Dados_atualizados(string nome, double salarioliq)
+        {
+            return
+                $"Nome do funcionario :{nome_funcionario}," +
+                $"Salario líquido: {salario_liquido}";
+
+        }
+    }
+
+
+}
+
+
+
---
+++ POO/Aula 11/Funcionarios/Program.cs
@@ -0,0 +1,29 @@
+﻿using funcionario;
+using static System.Console;
+
+WriteLine("Digite as informações do funcionário:");
+
+Write("Digite o nome do funcionário: ");
+string nome = ReadLine();
+Write("Digite o salário do funcionário: ");
+double salario = double.Parse(ReadLine());
+Write("Digite o valor do imposto sobre o salário funcionário: ");
+double imposto = double.Parse(ReadLine());
+
+WriteLine("Dados do funcionário");
+WriteLine($"Nome do funcionario: {nome}");
+WriteLine($"Salario do funcionario: {salario - imposto}");
+Dados f = new Dados(nome, salario, imposto);
+
+WriteLine("Digite a porcentagem de aumento do salário do funcionário: ");
+double porcentagem = double.Parse(ReadLine());
+WriteLine($"Salário do funcionário aumentado em: {f.aumento_porcentagem}%");
+
+f.aumento_porcentagem(porcentagem);
+f.salario_liquido(salario);
+f.Dados_atualizados(nome, salario);
+
+WriteLine($"Dados do funcionário atualizados!" +
+    $"{f.Dados_atualizados(nome, salario)}");
+
+
