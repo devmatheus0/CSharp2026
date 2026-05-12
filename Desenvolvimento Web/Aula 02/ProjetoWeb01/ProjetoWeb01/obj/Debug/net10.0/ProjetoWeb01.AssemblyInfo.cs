92f650eb4d9864da011666d96f2254615c1c9e65
pontelyasmin <pontelyasmin@gmail.com>
2026-05-11 21:50:23 -0300

    Aula 02 - Continuação do código

--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a098758e2fb789dbf91d208f52c2b69218b957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2417c2851a3a86b1289e851756d28c762bfe02")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
