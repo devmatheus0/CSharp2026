a78af34a62f9ea0fc0ec3ccb5fd3cf78de8c245b
devmatheus0 <Devmatheus0@gmail.com>
2026-05-15 20:50:24 -0300

    FRONT-END FINALIZADO!

--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f650eb4d9864da011666d96f2254615c1c9e65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742ace6f104ed0df160bd87e427aa8ef7bfb2e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
