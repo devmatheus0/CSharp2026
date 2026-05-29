a0b6639d460a2c67abb3b8db16ef2b431b04635c
devmatheus0 <Devmatheus0@gmail.com>
2026-05-29 20:50:16 -0300

    Continuação da AULA 02

--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/Consultas/SQLQuery01.sql
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/Consultas/SQLQuery01.sql
@@ -1,3 +1,5 @@
+
+/*
 --Inserir os dados da tabela aluno --
 INSERT INTO Alunos(RA, StatusWIFI, StatusAction, CursoID, Nome, Email, Senha, Regra)
 VALUES
@@ -5,4 +7,10 @@ VALUES
 (123457, 'Inativo', 'Aguardando aprovação', 2, 'Flavio', 'flavio@aluno.senaisp.br', 'F1c', 0),
 (123458, 'Inativo', 'Aguardando aprovação', 1, 'Naruto', 'naruto@aluno.senaisp.br', 'F1c', 0),
 (123459, 'Inativo', 'Aguardando aprovação', 2, 'Zorro', 'zorro@aluno.senaisp.br', 'F1c', 0);
+GO
+
+*/
+INSERT INTO Alunos(RA, StatusWIFI, StatusAction, CursoID, Nome, Email, Senha, Regra)
+VALUES
+(123460, 'Inativo', 'Aguardando aprovação', 1, 'Cesar', 'Cesar@aluno.senaisp.br', 'F1c', 0);
 GO
--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42db0ee7153161b9888f93fd13b60e01ed9ab8dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6743a4db4f3e132b234ba87b07ad6f0d4e9d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
